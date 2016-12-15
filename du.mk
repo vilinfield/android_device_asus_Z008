15eacd3054a0805222fa388cf559c9585a58bc5a
Victor Linfield <vilinfield@gmail.com>
2016-12-15 08:29:53 -0330

    make DU specific

--- du.mk
+++ du.mk
@@ -2,20 +2,20 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
+# Inherit some common DU stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/Z008/device.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/Z008/overlay
 
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_Z008
+PRODUCT_NAME := du_Z008
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_Z008
 PRODUCT_MANUFACTURER := asus
